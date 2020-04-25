f1ca2deebdb63d8db582f37b317fc730338dc7e8
dan_melnikov <dan_tm1994@mail.ru>
2020-04-25 16:14:00 +0300

    Изменен дефолтный layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,46 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <v-app>
+    <div class="main-wrapper">
+      <div class="navbar">
+        <Navigation />
+      </div>
+
+      <main class="page-content">
+        <nuxt />
+      </main> 
+    </div>
+  </v-app>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import Navigation from '../components/Navigation'
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+export default {
+  components: {
+    Navigation
+  }
 }
+</script>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+<style lang="less">
+  @import 'vuetify/dist/vuetify.min.css';
+  @import '@mdi/font/css/materialdesignicons.min.css';
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+  .main-wrapper {
+    display: grid;
+    grid-template-areas: "nav page";
+    grid-template-columns: 300px 1fr;
+    grid-template-rows: 100vh;
+  }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+  .navbar {
+    grid-area: nav;
+  }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .page-content {
+    grid-area: page;
+    padding: 20px 30px;
+    background-color: #fff3e0;
+    overflow: auto;
+  }
 </style>
